7761ca2a6801daa1079538587f0b4279666bfea5
çağrı çamyar <cagri-95@hotmail.com>
2024-06-11 15:36:26 +0300

    API kuruldu

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,131 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("getCarByBrandId")]
+        public IActionResult GetCarsByBrandId(int brandId) 
+        {
+            var result = _carService.GetCarsByBrandId(2);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("GetCarsByColourId")]
+        public IActionResult GetCarsByColourId(int colourId)
+        {
+            var result = _carService.GetCarsByColourId(4);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("GetByDailyPrice")]
+        public IActionResult GetByDailyPrice(int dailyPrice)
+        {
+            var result = _carService.GetByDailyPrice(400, 2000);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("NameMinTwoChars")]
+        public IActionResult NameMinTwoChars(Car car)
+        {
+            var result = _carService.NameMinTwoChars(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("DailyPriceMoreThanZero")]
+        public IActionResult DailyPriceMoreThanZero(Car car)
+        {
+            var result = _carService.DailyPriceMoreThanZero(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("GetCarDetails")]
+        public IActionResult GetCarDetails()
+        {
+            var result = _carService.GetCarDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpPost("AddCar")]
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
